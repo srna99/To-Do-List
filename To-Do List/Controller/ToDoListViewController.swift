151664c68e9355e7c65c4d73f47a5de1b3a22008
Serena Cheng <serena.ch1999@gmail.com>
2019-07-13 15:48:36 -0400

    Add search bar and search bar methods.

--- To-Do List/Controller/ToDoListViewController.swift
+++ To-Do List/Controller/ToDoListViewController.swift
@@ -118,9 +118,7 @@ class ToDoListViewController: UITableViewController {
         
     }
     
-    func loadItems() {
-
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request : NSFetchRequest<Item> = Item.fetchRequest()) {
         
         do {
             itemArray = try context.fetch(request)
@@ -129,7 +127,51 @@ class ToDoListViewController: UITableViewController {
             print("Error fetching data, ", error)
         }
         
+        tableView.reloadData()
+        
     }
     
 }
 
+//MARK: - Search Bar Methods
+
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchItems(through: searchBar)
+        
+        DispatchQueue.main.async {
+            searchBar.resignFirstResponder()
+        }
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text?.count == 0 {
+            
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+        else {
+            searchItems(through: searchBar)
+        }
+        
+    }
+    
+    func searchItems(through searchBar : UISearchBar) {
+        
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+        
+    }
+    
+}
