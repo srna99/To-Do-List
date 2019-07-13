f4fc48458ed4a2180e9cf10edc778a63ef517da2
Serena Cheng <serena.ch1999@gmail.com>
2019-07-13 12:19:11 -0400

    Add functionality to the add new items button and implement an alert.

--- To-Do List/ToDoListViewController.swift
+++ To-Do List/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["Buy apples", "Buy eggs", "Buy milk"]
+    var itemArray = ["Buy apples", "Buy eggs", "Buy milk"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,6 +47,42 @@ class ToDoListViewController: UITableViewController {
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addItemPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New To-Do Item", message: "", preferredStyle: .alert)
+        
+        let addAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        
+        alert.addAction(addAction)
+        alert.addAction(cancelAction)
+        alert.preferredAction = addAction
+        
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Create new item"
+            
+            textField = alertTextField
+            
+        }
+        
+        present(alert, animated: true, completion: nil)
         
     }
     
